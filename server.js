8feb9b99b9a77954ea073c88ccfd67c71ac7f83e
manInBlack749 <ilokisalem01@gmail.com>
2025-11-15 23:10:17 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express from "express";
+const fetch from "node-fetch";
+
+const app=express();
+
+app.get("/proxy",async(req,res)=>{
+      try{
+       const url=req.query.url;
+       if(!url)
+       res.status(404).json({error:"you missed the url"});
+       
+       const response= await fetch(url);
+       const data= await response.json();
+       
+       res.json(data);
+      } catch(err){
+        console.error(err);
+        res.status(500).json({error:"error with the socialcount");
+      } 
+});
+
+const PORT = process.env.PORT || 3000;
+app.listener(PORT,()=>{console.log("le serveur est en marche")})
